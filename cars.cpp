4e74b0973f6c10ed9e33097f25b4203ec0e828c7
RodolpheThienard <thienard.rodolphe1@gmail.com>
2024-01-17 12:37:26 +0100

    update v2

--- cars.cpp
+++ cars.cpp
@@ -1,211 +1,149 @@
+#include <cwchar>
 #include <iostream>
+#include <set>
 #include <stdio.h>
 #include <vector>
 
-// Chain of responsability
+/* Component */
+
+// TODO: a class for each component
+class Component
+{
+};
+
+/* Class Car */
+
+class Car
+{
+public:
+  Component roue;
+  Component chassis;
+  Component habitacle;
+  Component reservoir;
+  Component moteur;
+};
+
+/* Chain of responsability */
+
 class Handler
 {
 public:
   virtual ~Handler (){};
-
   void
   setSuccessor (Handler *s)
   {
     private_successor = s;
   }
-
   Handler *
   getSuccessor ()
   {
-    // std::cout << "Debut de la création de la voiture" << std::endl;
     return private_successor;
   }
-
   virtual void handleRequest () = 0;
 
 private:
   Handler *private_successor;
 };
 
-class Client
-{
-public:
-  Handler *h;
-};
-
-class Reservoir : public Handler
+class ProductionReservoir : public Handler
 {
 public:
   int capacite;
   void
   handleRequest ()
   {
-    if (!this->getSuccessor ())
-      {
-        std::cout << "Request handled by Concrete Handler 1 // no successor"
-                  << std::endl;
-      }
-    else
-      {
-        std::cout << "Ajout du Réservoir" << std::endl;
-        this->getSuccessor ()->handleRequest ();
-      }
+    std::cout << "Ajout du Réservoir" << std::endl;
+    Handler *succ = this->getSuccessor ();
+    if (succ)
+      succ->handleRequest ();
   }
 };
 
-class Habitacle : public Handler
+class ProductionHabitacle : public Handler
 {
 public:
   void
   handleRequest ()
   {
-    if (!this->getSuccessor ())
-      {
-        std::cout << "Request handled by Concrete Handler 1 // no successor"
-                  << std::endl;
-      }
-    else
-      {
-        std::cout << "Ajout de l'habitacle" << std::endl;
-        this->getSuccessor ()->handleRequest ();
-      }
+    std::cout << "Ajout de l'habitacle" << std::endl;
+    Handler *succ = this->getSuccessor ();
+    if (succ)
+      succ->handleRequest ();
   }
 };
 
-class Chassis : public Handler
+class ProductionChassis : public Handler
 {
 public:
   void
   handleRequest ()
   {
-    if (!this->getSuccessor ())
-      {
-        std::cout << "Request handled by Concrete Handler 1 // no successor"
-                  << std::endl;
-      }
-    else
-      {
-        std::cout << "ajout du chassis" << std::endl;
-        this->getSuccessor ()->handleRequest ();
-      }
+    std::cout << "ajout du chassis" << std::endl;
+    Handler *succ = this->getSuccessor ();
+    if (succ)
+      succ->handleRequest ();
   }
 };
 
-class Roue : public Handler
+class ProductionRoue : public Handler
 {
 public:
   void
   handleRequest ()
   {
-    if (!this->getSuccessor ())
-      {
-        std::cout << "Fin du véhicule" << std::endl;
-      }
-    else
-      {
-        std::cout << "Ajout des roues" << std::endl;
-        this->getSuccessor ()->handleRequest ();
-      }
+    std::cout << "Ajout des roues" << std::endl;
+    Handler *succ = this->getSuccessor ();
+    if (succ)
+      succ->handleRequest ();
   }
 };
 
-class Porte : public Handler
+class ProductionPorte : public Handler
 {
 public:
   bool fenetre_electrique;
   void
   handleRequest ()
   {
-    if (!this->getSuccessor ())
-      {
-        std::cout << "Request handled by Concrete Handler 1 // no successor"
-                  << std::endl;
-      }
-    else
-      {
-        std::cout << "ajout des portes" << std::endl;
-        this->getSuccessor ()->handleRequest ();
-      }
+    std::cout << "ajout des portes" << std::endl;
+    Handler *succ = this->getSuccessor ();
+    if (succ)
+      succ->handleRequest ();
   }
 };
 
-class Moteur : public Handler
+class ProductionMoteur : public Handler
 {
 public:
   int consommation;
   virtual void handleRequest () = 0;
 };
 
-class Moteur_sport : public Moteur
+class ProductionMoteurSport : public ProductionMoteur
 {
   void
   handleRequest ()
   {
-    if (!this->getSuccessor ())
-      {
-        std::cout << "Request handled by Concrete Handler 1 // no successor"
-                  << std::endl;
-      }
-    else
-      {
-        std::cout << "ajout du moteur" << std::endl;
-        this->getSuccessor ()->handleRequest ();
-      }
+    std::cout << "ajout du moteur sport" << std::endl;
+    Handler *succ = this->getSuccessor ();
+    if (succ)
+      succ->handleRequest ();
   }
 };
 
-class Moteur_citadine : public Moteur
+class ProductionMoteurCitadine : public ProductionMoteur
 {
   void
   handleRequest ()
   {
-    if (!this->getSuccessor ())
-      {
-        std::cout << "Request handled by Concrete Handler 1 // no successor"
-                  << std::endl;
-      }
-    else
-      {
-        std::cout << "ajout du moteur" << std::endl;
-        this->getSuccessor ()->handleRequest ();
-      }
+    std::cout << "ajout du moteur citadine" << std::endl;
+    Handler *succ = this->getSuccessor ();
+    if (succ)
+      succ->handleRequest ();
   }
 };
 
-// class Facade
-// {
-// public:
-//   Facade ()
-//   {
-//     std::cout << "Construction facade" << std::endl;
-//     porte = new Porte ();
-//     moteur = new Moteur ();
-//     chassis = new Chassis ();
-//     habitacle = new Habitacle ();
-//     roue = new Roue ();
-//     reservoir = new Reservoir ();
-//   }
-//   void
-//   wrapper ()
-//   {
-//     porte->suboperation ();
-//     chassis->suboperation ();
-//     habitacle->suboperation ();
-//     roue->suboperation ();
-//     reservoir->suboperation ();
-//     moteur->suboperation ();
-//   }
-
-// private:
-//   Porte *porte;
-//   Chassis *chassis;
-//   Habitacle *habitacle;
-//   Roue *roue;
-//   Reservoir *reservoir;
-//   Moteur *moteur;
-// };
-
-// Singleton
+/* Singleton */
 
 class Singleton
 {
@@ -251,136 +189,267 @@ private:
 // global variable
 Singleton *Singleton::unique_instance = nullptr;
 
-// Observer
+/* Parking */
 
-class Subject;
+class Parking
+{
+public:
+  Parking () : max_size (100), size (0) {}
+  Parking (const size_t size) : max_size (size), size (0) {}
+  ~Parking () {}
 
+  void
+  add_car ()
+  {
+    std::cout << "Voiture ajoutée au parking" << std::endl;
+    size++;
+  }
+  void
+  sell_car ()
+  {
+    std::cout << "Voiture retirée du parking" << std::endl;
+    size--;
+  }
+  bool
+  is_full ()
+  {
+    return size == max_size;
+  }
+
+private:
+  size_t max_size, size;
+};
+
+/* Abstract Factory */
+
+class AbstractFactory
+{
+public:
+  virtual ~AbstractFactory () {}
+  virtual Car *factoryMethod () = 0;
+
+  Parking *parking = nullptr;
+};
+
+class ConcreteFactory : public AbstractFactory
+{
+public:
+  enum class CarType
+  {
+    CITY,
+    SPORT
+  };
+  CarType car_type;
+
+  ConcreteFactory (CarType type, Parking *parking) : car_type (type)
+  {
+    std::cout << "ConcreteFactory" << std::endl;
+    parking = parking;
+    initHandler ();
+  }
+
+  void
+  initHandler ()
+  {
+    auto handler_roue = new ProductionRoue ();
+    auto handler_chassis = new ProductionChassis ();
+    auto handler_porte = new ProductionPorte ();
+    auto handler_moteur = new ProductionMoteurSport ();
+    auto handler_habitacle = new ProductionHabitacle ();
+    auto handler_reservoir = new ProductionReservoir ();
+
+    handler_city = handler_roue;
+    handler_roue->setSuccessor (handler_chassis);
+    handler_chassis->setSuccessor (handler_porte);
+    handler_porte->setSuccessor (handler_moteur);
+    handler_moteur->setSuccessor (handler_habitacle);
+    handler_habitacle->setSuccessor (handler_reservoir);
+
+    handler_sport = handler_city;
+  }
+
+  Car *
+  factoryMethod ()
+  {
+    Car *car = new Car ();
+    switch (car_type)
+      {
+      case CarType::CITY:
+        std::cout << "ConcreteFactory : CITY" << std::endl;
+        handler_city->handleRequest ();
+        break;
+      case CarType::SPORT:
+        std::cout << "ConcreteFactory : SPORT" << std::endl;
+        handler_city->handleRequest ();
+        break;
+      }
+    return car;
+  }
+
+private:
+  Handler *handler_city;
+  Handler *handler_sport;
+};
+
+/* Observer */
 class Observer
 {
 public:
-  Observer (const int id) : p_id (id) {}
+  Observer () {}
   virtual ~Observer () {}
-
-  virtual int getState () = 0;
-  virtual void update (Subject *subject) = 0;
-  virtual int getId () = 0;
-
-protected:
-  int p_id;
+  virtual void update () = 0;
 };
 
-class ConcreteObserver : public Observer
+constexpr const size_t end_of_experiment = 200;
+
+/* Agenda */
+class Agenda
 {
 public:
-  ConcreteObserver (const int state, const int id)
-      : observerState (state), Observer (id)
+  Agenda (const Agenda &) = delete;
+  Agenda &operator= (const Agenda &) = delete;
+  static Agenda *
+  instance ()
   {
+    if (agenda == nullptr)
+      agenda = new Agenda;
+    return agenda;
   }
-  ~ConcreteObserver () {}
-
-  int
-  getState ()
-  {
-    return observerState;
-  }
-
-  int
-  getId ()
-  {
-    return p_id; // protected
-  }
-
-  void update (Subject *subject);
-
-private:
-  int observerState;
-};
-
-class Subject
-{
-public:
-  virtual ~Subject () {}
-
   void
-  attach (Observer *observer)
+  notify ()
   {
-    vObserver.emplace_back (observer);
-  }
-
-  void
-  detach (const int id)
-  {
-    for (auto it = vObserver.begin (); it != vObserver.end (); ++it)
+    if (_today >= end_of_experiment)
+      return;
+    nb_ends = 0;
+    std::cout << "Date: " << _today << std::endl;
+    if (_working_days.contains (_today))
       {
-        if ((*it)->getId () == id)
+        for (auto obs = _observers.cbegin (); obs != _observers.cend (); obs++)
           {
-            vObserver.erase (it);
+            (*obs)->update ();
+          }
+      }
+    else
+      {
+        _today++;
+        notify ();
+      }
+  }
+  void
+  end_of_day (void)
+  {
+    nb_ends++;
+    if (nb_ends == _observers.size ())
+      {
+        _today++;
+        notify ();
+      }
+  }
+  void
+  attach_observer (Observer *obs)
+  {
+    _observers.push_back (obs);
+  }
+  void
+  detach_observer (Observer *obs)
+  {
+    for (auto o = _observers.cbegin (); o != _observers.cend (); o++)
+      {
+        if (*o == obs)
+          {
+            _observers.erase (o);
           }
       }
   }
 
-  void
-  notify ()
+private:
+  Agenda ()
   {
-    for (auto observer : vObserver)
+    _today = 0;
+    nb_ends = 0;
+    _working_days = std::set<size_t> ();
+    for (size_t i = 0; i < end_of_experiment; i++)
       {
-        observer->update (this);
+        // No production on weekends
+        if (!(i % 7 == 6 || i % 7 == 5))
+          {
+            _working_days.insert (i);
+          }
+      }
+  }
+  size_t _today;
+  size_t nb_ends;
+  std::set<size_t> _working_days;
+  std::vector<Observer *> _observers;
+  static Agenda *agenda;
+};
+
+Agenda *Agenda::agenda = Agenda::instance ();
+
+class ConcreteObserver : public Observer
+{
+public:
+  ConcreteObserver (Parking *p) : parking (p) {}
+  ~ConcreteObserver () {}
+
+  void update (void);
+  void
+  add_factory (AbstractFactory *f)
+  {
+    factories.push_back (f);
+  }
+  void
+  delete_factory (AbstractFactory *f)
+  {
+    for (auto o = factories.cbegin (); o != factories.cend (); o++)
+      {
+        if (*o == f)
+          {
+            factories.erase (o);
+          }
       }
   }
 
-  virtual int getState () = 0;
-  virtual void setState (const int state) = 0;
-
 private:
-  std::vector<Observer *> vObserver;
-};
-
-class ConcreteSubject : public Subject
-{
-public:
-  ~ConcreteSubject () {}
-
-  int
-  getState ()
-  {
-    return subjectState;
-  }
-
-  void
-  setState (const int state)
-  {
-    subjectState = state;
-  }
-
-private:
-  int subjectState;
+  Parking *parking;
+  std::vector<AbstractFactory *> factories;
+  void end_of_day (void);
 };
 
 void
-ConcreteObserver::update (Subject *subject)
+ConcreteObserver::update (void)
 {
-  observerState = subject->getState ();
-  std::cout << "Observer(id=" << p_id << ") update state to: " << observerState
-            << std::endl;
+  if (!parking->is_full ())
+    {
+      for (auto f = factories.cbegin (); f != factories.cend (); f++)
+        {
+          (*f)->factoryMethod ();
+        }
+    }
+  this->end_of_day ();
 }
+
+void
+ConcreteObserver::end_of_day (void)
+{
+  Agenda::instance ()->end_of_day ();
+}
+
+/* Main */
 
 int
 main ()
 {
-  Client *client = new Client ();
-  Chassis *chassis = new Chassis ();
-  Porte *porte = new Porte ();
-  Reservoir *reservoir = new Reservoir ();
-  Moteur *moteur = new Moteur_sport ();
-  Roue *roue = new Roue ();
-  Habitacle *habitacle = new Habitacle ();
+  //
 
-  client->h = chassis;
-  chassis->setSuccessor (reservoir);
-  reservoir->setSuccessor (moteur);
-  moteur->setSuccessor (habitacle);
-  habitacle->setSuccessor (porte);
-  porte->setSuccessor (roue);
+  Parking *parking = new Parking ();
+  ConcreteObserver obs = ConcreteObserver (parking);
+  AbstractFactory *factory
+      = new ConcreteFactory (ConcreteFactory::CarType::CITY, parking);
+  obs.add_factory (factory);
+  Agenda::instance ()->attach_observer (&obs);
+  Agenda::instance ()->notify ();
 
-  client->h->handleRequest ();
+  //
+  /* auto car = factory->factoryMethod(); */
 }
